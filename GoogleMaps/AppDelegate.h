5d22f0595f24a1c2c0d4b94f76795dd97efd8116
Bala29 <balamurugan2999@gmail.com>
2017-08-07 22:31:15 +0530

    Initial Commit

---
+++ GoogleMaps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleMaps
+//
+//  Created by Bala on 07/08/17.
+//  Copyright © 2017 Erabala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
